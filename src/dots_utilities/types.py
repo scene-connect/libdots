5996e0c1b630e98daea44d2df20b74237e9448b1
Dolf Andringa <dolfandringa@gmail.com>
2025-01-21 15:17:30 -0500

    Add data inventory and mqtt classes

---
+++ src/dots_utilities/types.py
@@ -0,0 +1,24 @@
+#  This work is based on original code developed and copyrighted by TNO 2023.
+#  Subsequent contributions are licensed to you by the developers of such code and are
+#  made available to the Project under one or several contributor license agreements.
+#
+#  This work is licensed to you under the Apache License, Version 2.0.
+#  You may obtain a copy of the license at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Contributors:
+#      TNO         - Initial implementation
+#  Manager:
+#      TNO
+
+
+from esdl import DataSource
+from esdl import EnergySystem
+from esdl import GenericProfile
+from esdl import Item
+
+EsdlId = str
+ServiceName = str
+
+ESDLObject = Item | EnergySystem | GenericProfile | DataSource
