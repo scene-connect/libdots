9d19eb2d9921ad38602adbfd021fa03243cab1a4
Dolf Andringa <dolfandringa@gmail.com>
2025-02-01 17:44:21 -0500

    Add service configuration to dots_utilities.model.config and use taht in the service

--- src/dots_utilities/model/service.py
+++ src/dots_utilities/model/service.py
@@ -6,21 +6,22 @@ from typing import Any
 from dots_utilities.io.input_data_inventory import InputDataInventory
 from dots_utilities.io.mqtt_client import MqttClient
 from dots_utilities.io.mqtt_log_handler import MqttLogHandler
+from dots_utilities.model.config import ServiceConfig
 from dots_utilities.model.service_calc import CalculationFunction
 from dots_utilities.model.service_calc import ServiceCalc
 
 
 class BaseService(ABC):
-    def __init__(self, config: dict[str, Any]):
+    def __init__(self, config: ServiceConfig):
         # initialize input data container and service calc
         service_calc = self.service_calc_class(
-            simulation_id=config["SIMULATION_ID"],
-            model_id=config["MODEL_ID"],
-            influxdb_host=config["INFLUXDB_HOST"],
-            influxdb_port=config["INFLUXDB_PORT"],
-            influxdb_user=config["INFLUXDB_USER"],
-            influxdb_password=config["INFLUXDB_PASSWORD"],
-            influxdb_name=config["INFLUXDB_NAME"],
+            simulation_id=config.simulation_id,
+            model_id=config.model_id,
+            influxdb_host=config.influxdb_host,
+            influxdb_port=config.influxdb_port,
+            influxdb_user=config.influxdb_user,
+            influxdb_password=config.influxdb_password.get_secret_value(),
+            influxdb_name=config.influxdb_name,
         )
         self.logger = logging.getLogger()
         mqtt_handler = MqttLogHandler(self.mqtt_client)
@@ -32,11 +33,11 @@ class BaseService(ABC):
 
         # initialize mqtt client
         self.mqtt_client = MqttClient(
-            host=config["MQTT_HOST"],
-            port=config["MQTT_PORT"],
-            qos=config["MQTT_QOS"],
-            username=config["MQTT_USERNAME"],
-            password=config["MQTT_PASSWORD"],
+            host=config.mqtt_host,
+            port=config.mqtt_port,
+            qos=config.mqtt_qos,
+            username=config.mqtt_username,
+            password=config.mqtt_password.get_secret_value(),
             input_data_inventory=input_data_inventory,
             service_calc=service_calc,
             service_name=service_calc.service_name,
