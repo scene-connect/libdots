8afa5bfd08a7e32b1d63afc3afdc9c22d489e4cf
Dolf Andringa <dolfandringa@gmail.com>
2025-02-12 11:21:04 -0500

    Make foss ready (#18)
    * Add license information
    * Update ci to python 3.12
    * Publish to pypi and remove our private repositories

---
+++ src/libdots/__init__.py
@@ -0,0 +1,15 @@
+#  This work is based on original code developed and copyrighted by TNO 2023
+#  and further developed and copyrighted by Scene Ltd in 2025.
+#  Subsequent contributions are licensed to you by the developers of such code and are
+#  made available under one or several contributor license agreements.
+#
+#  This work is licensed to you under the Apache License, Version 2.0.
+#  You may obtain a copy of the license at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Contributors:
+#      TNO         - Initial implementation of the dots calculation-service-generator
+#      Scene Ltd   - Development of libdots
+#  Manager:
+#      Scene Ltd
